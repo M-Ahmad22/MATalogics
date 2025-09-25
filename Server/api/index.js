e27fac0f5e9d9291d922e0359e68bb98c081f1cb
Muhammad Ahmad Tahir <muhammadahmadtahir19@gmail.com>
2025-09-26 03:59:13 +0500

    Initial commit with Admin, Client, Server structure and .gitignore

---
+++ Server/api/index.js
@@ -0,0 +1,8 @@
+const app = require("../App");
+const PORT = process.env.PORT;
+
+// app.listen(PORT, () => {
+//   console.log(`Backend Server is running on port ${PORT}`);
+// });
+
+module.exports = app;
--- Server/server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./App");
-const PORT = process.env.PORT;
-
-app.listen(PORT, () => {
-  console.log(`Backend Server is running on port ${PORT}`);
-});
-
-module.exports = app;
