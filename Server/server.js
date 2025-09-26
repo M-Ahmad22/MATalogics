d2919c0dcb19e2c800e2853741ece31d737a58c9
Muhammad Ahmad Tahir <muhammadahmadtahir19@gmail.com>
2025-09-26 10:52:57 +0500

    Backend Deployment v.1.0

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,9 @@
 const app = require("./App");
-// const PORT = process.env.PORT;
+const PORT = process.env.PORT;
 
-// app.listen(PORT, () => {
-//   console.log(`Backend Server is running on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Backend Server is running on port ${PORT}`);
+});
 
 // module.exports = app;
 
