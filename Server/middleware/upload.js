0ba83aa9b3f1f3095b11aecba0e7c249873a7198
Muhammad Ahmad Tahir <muhammadahmadtahir19@gmail.com>
2025-09-26 11:29:33 +0500

    Backend Deployment v.1.0

--- Server/middleware/upload.js
+++ Server/middleware/upload.js
@@ -2,20 +2,19 @@ const multer = require("multer");
 const fs = require("fs");
 const path = require("path");
 
-// Destination function for serverless
+// Use /tmp folder for serverless
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    const uploadPath = "/tmp/uploads";
+    const uploadPath = "/tmp/uploads"; // ✅ Must be /tmp
     if (!fs.existsSync(uploadPath)) {
       fs.mkdirSync(uploadPath, { recursive: true });
     }
     cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+    cb(null, file.originalname); // Keep original name
   },
 });
 
 const upload = multer({ storage });
-
 module.exports = upload;
