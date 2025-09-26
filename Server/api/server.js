c069f80e90d112d8a58fa012bd4dee9575439849
Muhammad Ahmad Tahir <muhammadahmadtahir19@gmail.com>
2025-09-26 11:11:40 +0500

    Backend Deployment v.1.0

--- Server/api/server.js
+++ Server/api/server.js
@@ -1,17 +1,4 @@
-const app = require("../App");
-// const PORT = process.env.PORT;
-
-// app.listen(PORT, () => {
-//   console.log(`Backend Server is running on port ${PORT}`);
-// });
-
-// module.exports = app;
-
-// module.exports = (req, res) => {
-//   app(req, res);
-// };
-
 const serverless = require("serverless-http");
-// const app = require("./App");
+const app = require("../App");
 
 module.exports = serverless(app);
