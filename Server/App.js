aa6ca911ade42f0f8ccac8821ca7de450b3105f0
Muhammad Ahmad Tahir <muhammadahmadtahir19@gmail.com>
2025-09-29 00:16:50 +0500

    Updated admin path

--- Server/App.js
+++ Server/App.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL, process.env.ADMIN_FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
